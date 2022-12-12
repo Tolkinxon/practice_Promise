274ac8db4d15d70d6759ba7218894c6b8b0e46d5
Tolkinxon <krosstony639@gmail.com>
2022-12-12 16:47:40 +0500

    second appearance of promise

--- promise.js
+++ promise.js
@@ -1,21 +1,36 @@
 "use strict"
 
-const result = false
+// const result = false
 
-const promisePractise = new Promise((resolve, reject) => {
+// const promisePractise = new Promise((resolve, reject) => {
 
-if (result) {
-    const response = 200
-    resolve(response)
-}
-else{
-    const respons = 404
-    reject(respons)
-}
+// if (result) {
+//     const response = 200
+//     resolve(response)
+// }
+// else{
+//     const respons = 404
+//     reject(respons)
+// }
+// })
+
+
+// promisePractise.then((data) => {console.log(data);}).catch((information) => {console.log(information);})
+
+
+
+
+
+const res = new Promise((resolve) => {
+    console.log('request data');
+
+    const car = {
+        color: 'red',
+        height: 1.3
+    }
+
+    resolve(car)
 })
 
-
-promisePractise.then((data) => {console.log(data);}).catch((information) => {console.log(information);})
-
-
+res.then((data) => {console.log(data)})
 
